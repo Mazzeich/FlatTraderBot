18642e045a176e523573cf60d8744358df769ebb
Mazzeich <madnessfox@yandex.ru>
2020-09-23 17:20:23 +0500

    HistoricalFlatFinder.cs

--- HistoricalFlatFinder.cs
+++ HistoricalFlatFinder.cs
@@ -40,62 +40,76 @@ namespace Lua
             {
                 aperture.Add(globalCandles[i]);
             }
-            Console.WriteLine("Стартовое окно: globalCandles.Length = {0}\taperture.Count = {1}", globalCandles.Count, aperture.Count);
+            Console.WriteLine("Стартовое окно: globalCandles.Count = {0}\taperture.Count = {1}", globalCandles.Count, aperture.Count);
 
             FindAllFlats();
         }
 
         private void FindAllFlats()
         {
-            Console.WriteLine("[FindAllFlats()]");
-
             int overallAdded = 0;
             int localAddedCandles = 0;
             int step = 0;
 
-            for (int i = 0; i < globalCandles.Count - 1; i += (_Constants.NAperture * step) + overallAdded)
+            for (int i = 0; i < globalCandles.Count - _Constants.NAperture - 1; i += _Constants.NAperture + overallAdded)
             {
                 step++;
                 localAddedCandles = 0;
-                Console.WriteLine("[i] = {0}\t[aperture.Count] = {1}", i, aperture.Count);
-                if (globalCandles.Count - i <= _Constants.NAperture) // Если в конце осталось меньше свечей, чем вмещает окно
+                Console.WriteLine("[i] = {0}\t\t[aperture.Count] = {1}", i, aperture.Count);
+                
+                // Если в конце осталось меньше свечей, чем вмещает окно
+                if (globalCandles.Count - (_Constants.NAperture * step) + overallAdded <= _Constants.NAperture)
                 {
                     break;
                 }
 
-
                 FlatIdentifier flatIdentifier = new FlatIdentifier(aperture);
-                flatIdentifier.isFlat = false;
 
                 flatIdentifier.Identify();
-
-                while (flatIdentifier.isFlat)
+                // Если не нашли боковик сходу
+                if (flatIdentifier.IsFlat == false)
                 {
+                    // Двигаем окно в следующую позицию
+                    aperture = MoveAperture(overallAdded, step);
+                    continue;
+                }
+                
+                while (flatIdentifier.IsFlat == true)
+                {                
+
                     localAddedCandles++;
+                    // Расширяем окно
                     aperture.Add(globalCandles[(aperture.Count * step) - 1 + localAddedCandles]);
                     Console.WriteLine("Aperture expanded...\t[aperture.Count] = {0}", aperture.Count);
                     flatIdentifier.Identify();
                     
-                    if (flatIdentifier.isFlat == false)
+                    if (flatIdentifier.IsFlat == false)
                     {
                         flatsFound++;
-                        overallAdded += localAddedCandles;
+                        // Нужно отсечь последнюю добавленную свечу,
+                        // так как на ней `isFlat == false`
+                        overallAdded += localAddedCandles - 1;
 
                         Console.WriteLine("+1 боковик!");
+                        aperture.RemoveAt(aperture.Count - 1);
                         Console.WriteLine("[overallAdded] = {0}", overallAdded);
                         Bounds bounds;
-                        bounds.start = flatIdentifier.Bounds.start;
-                        bounds.end = flatIdentifier.Bounds.end;
+                        // bounds.left = flatIdentifier.FlatBounds.left;
+                        // bounds.right = flatIdentifier.FlatBounds.right;
+                        bounds = flatIdentifier.SetBounds(aperture[0], aperture[^1]);
                         apertureBounds.Add(bounds);
+                        Printer printer  = new Printer(flatIdentifier);
+                        printer.OutputApertureInfo();
+                        // Двигаем окно в следующую позицию
+                        aperture = MoveAperture(overallAdded, step);
                     }
                 }
 
-                aperture = MoveAperture(overallAdded, step);
             }
         }
 
         /// <summary>
-        /// Функция перемещения окна в следующую позцию
+        /// Функция перемещения окна в следующую позицию
         /// </summary>
         /// <param name="_candlesToAdd">Всего свечей, которые были добавлены ранее</param>
         /// <param name="_step">Текущий шаг прохода алгоритма</param>
@@ -104,15 +118,13 @@ namespace Lua
         {
             Console.WriteLine("[MoveAperture()]");
             aperture.Clear();
-
             
-            int startPosition = (_Constants.NAperture * _step) + _candlesToAdd + 1;
+            int startPosition = (_Constants.NAperture * _step) + _candlesToAdd;
             for (int i = startPosition; i < startPosition + _Constants.NAperture; i++)
             {
                 aperture.Add(globalCandles[i]);
             }
-
-
+            
             return aperture;
         }
     }
