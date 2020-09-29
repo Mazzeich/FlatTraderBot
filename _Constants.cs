48a471f9906e55d7850922164a4902f97b38bf78
Mazzeich <madnessfox@yandex.ru>
2020-09-29 17:39:24 +0500

    _Constants.cs included into namespace Lua

--- _Constants.cs
+++ _Constants.cs
@@ -1,22 +1,23 @@
-using System;
-
-/// <summary>
-/// Структура постоянных величин
-/// </summary>
-public struct _Constants
+namespace Lua
 {
-    /// <value> Размер окна </value>
-    public const int NAperture = 40;
+    /// <summary>
+    /// Структура постоянных величин
+    /// </summary>
+    public struct _Constants
+    {
+        /// <value> Размер окна </value>
+        public const int NAperture = 40;
     
-    /// <value> Минимальная ширина коридора (коэфф. от цены инструмента)</value>
-    public const double MinWidthCoeff = 0.002;
+        /// <value> Минимальная ширина коридора (коэфф. от цены инструмента)</value>
+        public const double MinWidthCoeff = 0.002;
 
-    /// <value> Коэффициент для определения поведения тренда</value>
-    public const double KOffset = 0.01;
+        /// <value> Коэффициент для определения поведения тренда</value>
+        public const double KOffset = 0.01;
 
-    /// <value> Возможное отклонение экстремума от линии СКО</value>
-    public const double SDOffset = 0.0006;
+        /// <value> Возможное отклонение экстремума от линии СКО</value>
+        public const double SDOffset = 0.0006;
 
-    /// <value> Количество фазовых свечей, которые не нужно учитывать при вычислении уголового коэффициента </value>
-    public const double PhaseCandlesCoeff = 0.05;
+        /// <value> Количество фазовых свечей, которые не нужно учитывать при вычислении уголового коэффициента </value>
+        public const double PhaseCandlesCoeff = 0.05;
+    }
 }
