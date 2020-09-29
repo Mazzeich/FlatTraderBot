db3e5e0bb69f7856e6a298c4d2a9d4afb302314b
Mazzeich <madnessfox@yandex.ru>
2020-09-29 17:39:11 +0500

    _CandleStruct.cs included into namespace Lua

--- _CandleStruct.cs
+++ _CandleStruct.cs
@@ -1,18 +1,20 @@
-using System;
-/// <summary>
-/// Структура свечи (high, low, close, avg, date)
-/// </summary>
-public struct _CandleStruct
+namespace Lua
 {
-    /// <value> Хай текущей свечи </value>
-    public double high;
-    /// <value> Лоу текущей свечи </value>
-    public double low;
-    /// <value> Цена закрытия текущей свечи </value>
-    public double close;
-    /// <value> Средняя цена по свече (= (хай - лоу)/*0.5) </value>
-    public double avg;
+    /// <summary>
+    /// Структура свечи (high, low, close, avg, date)
+    /// </summary>
+    public struct _CandleStruct
+    {
+        /// <value> Хай текущей свечи </value>
+        public double high;
+        /// <value> Лоу текущей свечи </value>
+        public double low;
+        /// <value> Цена закрытия текущей свечи </value>
+        public double close;
+        /// <value> Средняя цена по свече (= (хай - лоу)/*0.5) </value>
+        public double avg;
     
-    /// <value> Дата закрытия свечи </value>
-    public string date;
+        /// <value> Дата закрытия свечи </value>
+        public string date;
+    }
 }
