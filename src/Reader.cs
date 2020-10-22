c1024ce7d369fd0f1e6952ebebd1c3023d0029e2
Mazzeich <madnessfox@yandex.ru>
2020-10-22 18:01:58 +0500

    Убрал лишний обсолютный (:}) код. При надобности сделаю заново

--- src/Reader.cs
+++ src/Reader.cs
@@ -46,51 +46,9 @@ namespace FlatTraderBot
             currentDirectory = Directory.GetCurrentDirectory();
         }
 
-        
-
         ~Reader()
         {
             GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Считать все строки из файлов распарсенных данных
-        /// </summary>
-        [Obsolete("Method were used to work with dataParser.lua")]
-        public List<_CandleStruct> GetSeparatedData()
-        {
-            logger.Trace("[GetSeparatedData()] invoked. Reading data...");
-            pathHigh   = Path.Combine(currentDirectory, @"..\..\..\Data\dataHigh.txt");
-            pathLow    = Path.Combine(currentDirectory, @"..\..\..\Data\dataLow.txt");
-            pathAvg    = Path.Combine(currentDirectory, @"..\..\..\Data\dataAvg.txt");
-            pathOpen   = Path.Combine(currentDirectory, @"..\..\..\Data\dataOpen.txt");
-            pathClose  = Path.Combine(currentDirectory, @"..\..\..\Data\dataClose.txt");
-            pathVolume = Path.Combine(currentDirectory, @"..\..\..\Data\dataVolume.txt");
-
-            readHeights = File.ReadAllLines(pathHigh);
-            readLows    = File.ReadAllLines(pathLow);
-            readAvgs    = File.ReadAllLines(pathAvg);
-            readOpens   = File.ReadAllLines(pathOpen);
-            readCloses  = File.ReadAllLines(pathClose);
-            readVolumes = File.ReadAllLines(pathVolume);
-        
-            for (int i = 0; i < readHeights.Length; i++) //readHeights.Length = readLows.Length
-            {
-                _CandleStruct temp;
-                temp.index = i;
-                temp.low   = double.Parse(readLows[i]   , CultureInfo.InvariantCulture);
-                temp.high  = double.Parse(readHeights[i], CultureInfo.InvariantCulture);
-                temp.close = double.Parse(readCloses[i] , CultureInfo.InvariantCulture);
-                temp.open  = double.Parse(readOpens[i], CultureInfo.InvariantCulture);
-                temp.avg   = double.Parse(readAvgs[i]   , CultureInfo.InvariantCulture);
-                temp.date  = "";
-                temp.time  = "";
-
-                candleStruct.Add(temp);
-            }
-
-            logger.Trace("[GetSeparatedData() finished]");
-            return candleStruct;
         }
 
         /// <summary>
