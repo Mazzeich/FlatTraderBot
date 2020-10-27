4af9997837eec65d8c6aa29aac4ce0e0d7f16be5
Mazzeich <madnessfox@yandex.ru>
2020-10-27 21:17:24 +0500

    Документация к структуре дальнего пробоя

--- src/_Breakthrough.cs
+++ src/_Breakthrough.cs
@@ -1,9 +1,21 @@
 ﻿namespace FlatTraderBot
 {
+	/// <summary>
+	/// Структура свечи дальнего пробоя после боковика
+	/// </summary>
 	public struct _Breakthrough
 	{
+		/// <summary>
+		/// Сама свеча пробоя
+		/// </summary>
 		public _CandleStruct candle;
+		/// <summary>
+		/// Расстояние в свечах между свечой закрытия и самой свечой пробоя
+		/// </summary>
 		public int distanceToClose;
+		/// <summary>
+		/// Разница в цене между ценой закрытия свечи закрытия боковика и самой свечой пробоя
+		/// </summary>
 		public double deltaPriceBreakthroughToClose;
 	}
 }
